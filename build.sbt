8e3344d3003f4b3733517ea13b9a4415ed1f48a9
alltonp <paulallton@gmail.com>
2015-01-23 21:53:11 +0000

    a little tidy up

--- build.sbt
+++ build.sbt
@@ -12,7 +12,7 @@ scalaVersion := "2.11.1"
 //seq(webSettings :_*)
 
 libraryDependencies ++= {
-  val liftVersion = "2.6-RC1"
+  val liftVersion = "2.6"
   Seq(
     "net.liftweb" %% "lift-webkit" % liftVersion % "compile",
     "org.eclipse.jetty" % "jetty-webapp" % "8.1.7.v20120910", // % "container",
--- src/main/scala/webserver/WebServer.scala
+++ src/main/scala/webserver/WebServer.scala
@@ -1,43 +1,5 @@
 package webserver
 
-//object WebServer extends App {
-//  val server = new Server(8080);
-//
-//  val context = new WebAppContext();
-//  context.setDescriptor(webapp+"/WEB-INF/web.xml");
-//  context.setResourceBase("../test-jetty-webapp/src/main/webapp");
-//  context.setContextPath("/");
-//  context.setParentLoaderPriority(true);
-//
-//  server.setHandler(context);
-//
-//  server.start();
-//  server.join();
-//
-//}
-
-//object WebServer2 extends App {
-//  import org.eclipse.jetty.server.Server
-//  import org.eclipse.jetty.webapp.WebAppContext
-//  import org.eclipse.jetty.server.handler.AllowSymLinkAliasChecker
-//
-//  val server = new Server(8080)
-//  val maxFormSize = 20 * 1000 * 1000
-//  server.setHandler(new WebAppContext("src/main/webapp", "/") {
-//    addAliasCheck(new AllowSymLinkAliasChecker)
-//  })
-//  try {
-//    server.start()
-////    if (args.contains("interruptible")) {
-////      Console.readLine("Press Enter to exit")
-////      server.stop()
-////    }
-//    server.join()
-//  }
-//  catch { case e: Throwable => e.printStackTrace(); System.exit(1) }
-//  finally System.exit(0)
-//}
-
 object WebServer extends App {
   import org.eclipse.jetty.server.Server
   import org.eclipse.jetty.server.nio.SelectChannelConnector
@@ -50,7 +12,6 @@ object WebServer extends App {
   private val context = createContext
   server.setHandler(context)
 
-
   private def createServer = {
     val server = new Server
     val selectChannelConnector = new SelectChannelConnector
@@ -59,21 +20,18 @@ object WebServer extends App {
     server
   }
 
-  def startServer = {
+  private def startServer() = {
     try {
-      println(">>> STARTING EMBEDDED JETTY SERVER")
       server.start()
-      println(">>> JETTY SERVER STARTED")
       while (!server.isRunning) Thread.sleep(100)
     } catch {
       case exception: Exception => {
-        println("FAILED TO START JETTY SERVER")
+        println("### failed to start jetty")
         exception.printStackTrace()
         throw exception
       }
     }
   }
-
 
   private def createContext = {
     val context = new WebAppContext()
@@ -89,67 +47,5 @@ object WebServer extends App {
     context
   }
 
-   startServer
+   startServer()
 }
-
-//import java.io.File
-//import org.eclipse.jetty.server.Server
-//import org.eclipse.jetty.server.nio.SelectChannelConnector
-//import org.eclipse.jetty.webapp.WebAppContext
-//import org.eclipse.jetty.servlet.ServletContextHandler
-//
-//class WebServer(port: Int, autoStart: Boolean = true, webAppPath: String = "src/main/webapp") {
-//  private val server = buildServer
-//  server.setHandler(createContext)
-//
-//  def add(path: String) {
-//    val context = new ServletContextHandler
-//    context.setServer(server)
-//    context.setContextPath(path)
-//  }
-//
-//  def start() {
-//    try {
-//      server.start()
-//      println("### Started web server on port %d...".format(port))
-//      while (!server.isStarted) Thread.sleep(100)
-//    } catch {
-//      case e: Exception => {
-//        println("### Failed to start web server on port %d".format(port))
-//        e.printStackTrace()
-//        throw e
-//      }
-//    }
-//  }
-//
-//  def stop() {
-//    server.stop()
-//    val end = System.currentTimeMillis() + 10000
-//    while (!server.isStopped && end > System.currentTimeMillis()) Thread.sleep(100)
-//    if (!server.isStopped) println("!!!!!!! SERVER FAILED TO STOP !!!!!!!")
-//  }
-//
-//  private def buildServer = {
-//    val server = new Server
-//    val scc = new SelectChannelConnector
-//    scc.setPort(port)
-//    scc.setAcceptors(Runtime.getRuntime.availableProcessors() * 2)
-//    scc.setResponseBufferSize(1000000)
-//    server.setConnectors(Array(scc))
-//    server.setStopAtShutdown(true)
-//    server
-//  }
-//
-//  private def createContext = {
-//    val context = new WebAppContext
-//    context.setServer(server)
-//    context.setContextPath("/")
-//    //TIP: jetty won't start if webapp directory doesnt exist - should probably try to create it
-//    if (new File(webAppPath).exists()) context.setWar(webAppPath) else context.setWar(getClass.getClassLoader().getResource("webapp").toExternalForm())
-//    context
-//  }
-//
-//  OnShutdown.execute("Stop web server", stop _)
-//
-//  if (autoStart) start()
-//}
