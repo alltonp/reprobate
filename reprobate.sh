065a662482330425a4324444da5144bcd0be7706
alltonp <paulallton@gmail.com>
2015-01-24 20:58:26 +0000

    try to handle stopping when it isnt running

--- reprobate.sh
+++ reprobate.sh
@@ -4,10 +4,13 @@ function start() {
     nohup java -cp reprobate.jar:$(echo lib/*.jar | tr ' ' ':') server.ReprobateServer > app.log 2>&1 &
 }
 
-#TODO: don't try to kill missing PID, results in: ps -ef | awk '/[R]eprobateServer/{print $2}'
 function stop() {
     PID=`currentpid`
-    echo "### Killing PID ${PID}...."
+    if [[ -n $PID ]]; then
+        echo "### Killing PID ${PID}...."
+    else
+        echo "### Reprobate wasn't running..."
+    fi
     kill $PID
 }
 
