230bd8c987159f33e5d5b72e550c785e795b6ff8
alltonp <paulallton@gmail.com>
2015-01-24 19:19:35 +0000

    even more tidy up

--- project/Build.scala
+++ project/Build.scala
@@ -1,22 +1,16 @@
+import sbt.Keys._
 import sbt._
-import Keys._
 
 object Reprobate extends Build {
   import BuildSettings._
-  import scala.collection._
 
-  lazy val root = Project(id = "reprobate", base = file("."), settings = standardBuildSettings ++ Seq())
+  lazy val root = Project(id = "reprobate", base = file("."), settings = standardBuildSettings)
 }
 
 object BuildSettings {
-
   val dist = taskKey[Unit]("dist")
 
   val standardBuildSettings: Seq[Def.Setting[_]] = Defaults.defaultSettings ++ Seq[Setting[_]](
-//      organization := "im.mange",     //TODO these 3 lines are ignored here and picked from build.sbt instead
-//      version := "1.0",
-//      scalaVersion := "2.11.4",
-
     mappings in (Compile, packageBin) ++= {
       val webapp: File = baseDirectory.value / "src/main/webapp"
       for ((from, to) <- (webapp ***) x rebase(webapp, "webapp")) yield (from, to)
@@ -34,20 +28,3 @@ object BuildSettings {
     }
   )
 }
-
-//object Resolvers {
-//  val jettyRepo = "jetty repo" at "http://siasia.github.com/maven2"
-//  val resolver = "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
-//}
-
-//object Dependency {
-//  private val jettyVersion = "8.1.7.v20120910"
-//  private val scalaTestVersion = "2.2.1"
-//
-//  val jetty = Seq (
-//      "org.eclipse.jetty" % "jetty-server" % jettyVersion,
-//      "org.eclipse.jetty" % "jetty-webapp" % jettyVersion
-//    )
-//
-//  val scalaTest = Seq("org.scalatest" % "scalatest_2.11" % scalaTestVersion  % "test")
-//}
