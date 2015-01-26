55676acdae097f558d5c43e6a7ae09526b0e1178
alltonp <paulallton@gmail.com>
2015-01-26 10:04:29 +0000

    update for curl

--- src/broadcast_example.sh
+++ src/broadcast_example.sh
@@ -3,4 +3,7 @@ VERSION="version"
 MACHINE_NAME="machine"
 DEPLOYER=`id -u -n`
 MESSAGE="(${APP}) ${VERSION} deployed to ${MACHINE_NAME} by ${DEPLOYER}"
-wget --timeout=15 --no-proxy -O- --post-data="{\"messages\":[\"${MESSAGE}\", \"Thats it\"]}" --header=Content-Type:application/json "http://localhost:8473/broadcast"
+
+#send using ...
+#wget --timeout=15 --no-proxy -O- --post-data="{\"messages\":[\"${MESSAGE}\"]}" --header=Content-Type:application/json "http://localhost:8473/broadcast"
+#curl --connect-timeout 15 -H "Content-Type: application/json" -d "{\"messages\":[\"${MESSAGE}\"]}" http://localhost:8473/broadcast
