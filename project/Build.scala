fdd6a4d64a29add00fb0029027c28c0da91dd7d8
alltonp <paulallton@gmail.com>
2015-01-26 16:41:00 +0000

    put reprobate.sh into package

--- project/Build.scala
+++ project/Build.scala
@@ -18,14 +18,8 @@ object BuildSettings {
       for ((from, to) <- (webapp ***) x rebase(webapp, "webapp")) yield (from, to)
     },
 
-    //TODO: give up and just use - https://github.com/xerial/sbt-pack
-
     dist <<= (baseDirectory, target, packageBin in Compile, dependencyClasspath in Compile) map {
       (base, targetDir, artifact, libs) =>
-        //TOOO: push jar into the lib dir
-        //TOOO: give jar version
-        //TODO: tidy up start script to remove jar name
-        //TODO: put version in dist name ...
         //TODO: apparently gzip will maintain permissions ...
         //TODO: exclude compiler jars etc
         val ver = Try(sys.env("TRAVIS_BUILD_NUMBER")).map("0.0." + _).getOrElse("1.0-SNAPSHOT")
@@ -34,7 +28,7 @@ object BuildSettings {
         IO.delete(distdir)
         IO.createDirectory(distdir)
         val jars = libs.map(_.data).pair(flatRebase(s"$dist/lib"))
-        val script = file("reprobate.sh").pair(flatRebase(dist))
+        val script = file("package/reprobate.sh").pair(flatRebase(dist))
         val files = Seq(artifact -> s"$dist/lib/$dist.jar")
         println(s"### Building $dist.zip")
         IO.zip(files ++ jars ++ script, targetDir / "dist.zip")
