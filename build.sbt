fd392d8eed455bdd35448ce7cb8d390cbe14969c
alltonp <paulallton@gmail.com>
2015-01-26 16:34:15 +0000

    use little-server

--- build.sbt
+++ build.sbt
@@ -9,13 +9,11 @@ version := Try(sys.env("TRAVIS_BUILD_NUMBER")).map("0.0." + _).getOrElse("1.0-SN
 scalaVersion := "2.11.4"
 
 libraryDependencies ++= {
-  val jettyVersion = "8.1.7.v20120910"
   Seq(
-    "org.eclipse.jetty" % "jetty-webapp" % jettyVersion,
-    "org.eclipse.jetty" % "jetty-server" % jettyVersion,
     "io.shaka" %% "naive-http" % "48",
     "ch.qos.logback" % "logback-classic" % "1.0.6",
     "im.mange" %% "jetboot" % "0.0.7",
+    "im.mange" %% "little-server" % "0.0.1",
     "im.mange" %% "shoreditch-api" % "0.0.60"
   )
 }
--- src/main/scala/server/ReprobateServer.scala
+++ src/main/scala/server/ReprobateServer.scala
@@ -1,47 +1,7 @@
 package server
 
-import java.io.File
-
-import org.eclipse.jetty.server.Server
-import org.eclipse.jetty.server.nio.SelectChannelConnector
-import org.eclipse.jetty.webapp.WebAppContext
-
-class WebServer(serverPort: Int) {
-  private val server = createServer(serverPort)
-  private val context = createContext
-  server.setHandler(context)
-
-  private def createServer(port: Int) = {
-    val server = new Server
-    val selectChannelConnector = new SelectChannelConnector
-    selectChannelConnector.setPort(port)
-    server.setConnectors(Array(selectChannelConnector))
-    server
-  }
-
-  private def startServer() = try {
-    server.start()
-  } catch {
-    case e: Throwable => e.printStackTrace(); throw e
-  }
-
-  private def createContext = {
-    def packagedPath(loader: ClassLoader, root: String) = loader.getResource(root).toExternalForm
-
-    def discover(path: String, packaged: String, context: WebAppContext) =
-      if (new File(path).exists()) path
-      else packagedPath(context.getClass.getClassLoader, "webapp")
-
-    val context = new WebAppContext()
-    context.setServer(server)
-    context.setContextPath("/")
-    context.setWar(discover("src/main/webapp", "webapp", context))
-    context
-  }
-
-  startServer()
-}
+import im.mange.little.LittleServer
 
 object ReprobateServer extends App {
-  new WebServer(8473)
+  new LittleServer(8473)
 }
