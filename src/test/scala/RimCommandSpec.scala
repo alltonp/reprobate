54ee425c73917e6a1a798a2226f5d35018772054
alltonp <paul.allton@theorem.technology>
2015-04-24 10:03:54 +0100

    replace big todo with a smaller one

--- src/test/scala/RimCommandSpec.scala
+++ src/test/scala/RimCommandSpec.scala
@@ -3,23 +3,7 @@ import org.scalatest.{MustMatchers, WordSpec}
 
 class RimCommandSpec extends WordSpec with MustMatchers {
 
-//TODO: start with happy path
-//  case In(Some(""), Nil) => onShowBoard(currentModel)
-//  case In(Some("help"), Nil) => onHelp(who, currentModel)
-//  case In(Some("?"), Nil) => onQueryIssues(currentModel, None)
-//  case In(Some("?"), List(query)) => onQueryIssues(currentModel, Some(query))
-//  case In(Some(ref), List("-")) => onRemoveIssue(ref, currentModel)
-//  case In(Some(ref), args) if args.nonEmpty && args.head == "=" => onEditIssue(ref, args.drop(1), currentModel)
-//  case In(Some(ref), List("/")) => onForwardIssue(who, ref, currentModel)
-//  case In(Some(ref), List("/!")) => onFastForwardIssue(who, ref, currentModel)
-//  case In(Some(ref), List(".")) => onBackwardIssue(who, ref, currentModel)
-//  case In(Some(ref), List(".!")) => onFastBackwardIssue(who, ref, currentModel)
-//  case In(Some(ref), List("@")) => onOwnIssue(who, ref, currentModel)
-//  case In(Some(ref), args) if args.nonEmpty && args.size > 1 && args.head == ":" => onTagIssue(ref, args.drop(1), currentModel)
-//  case In(Some(ref), args) if args.nonEmpty && args.size > 1 && args.head == ":-" => onDetagIssue(ref, args.drop(1), currentModel)
-//  case In(Some("release"), List(tag)) => onRelease(tag, currentModel)
-//  case In(Some("releases"), Nil) => onShowReleases(currentModel)
-//  case In(head, tail) => onUnknownCommand(head, tail)
+  //TODO: work out what examples are missing
 
   private val next = "next"
   private val doing = "doing"
