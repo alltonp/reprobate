9adb8aa1045143004d80d5111c01e56f8b47a5f7
alltonp <paul.allton@theorem.technology>
2015-05-22 09:24:43 +0100

    render dates in london time

--- src/main/scala/app/ServiceFactory.scala
+++ src/main/scala/app/ServiceFactory.scala
@@ -4,11 +4,12 @@ import app.server.ProbeProviderActor
 import im.mange.little.clock.{Clock, RealClock}
 import im.mange.little.date.DateFormatForHumans
 import net.liftweb.http._
+import org.joda.time.DateTimeZone
 
 object ServiceFactory extends Factory {
   lazy val probeProviderActorInstance: ProbeProviderActor = new ProbeProviderActor
   lazy val clockInstance: Clock = RealClock
-  lazy val dateFormatsInstance: DateFormatForHumans = new DateFormatForHumans(systemClock())
+  lazy val dateFormatsInstance: DateFormatForHumans = new DateFormatForHumans(systemClock(), DateTimeZone.forID("Europe/London"))
 
   implicit object probeProviderActor extends FactoryMaker(() ⇒ probeProviderActorInstance)
   implicit object systemClock extends FactoryMaker(() ⇒ clockInstance)
