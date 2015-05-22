b68817cb596d42e83e6673e280870869624057a9
alltonp <paul.allton@theorem.technology>
2015-05-22 08:56:45 +0100

    use clock religiously

--- src/main/scala/app/model/ActivePeriod.scala
+++ src/main/scala/app/model/ActivePeriod.scala
@@ -1,11 +1,11 @@
 package app.model
 
+import app.ServiceFactory.systemClock
 import org.joda.time.{DateTime, LocalDateTime}
 
 case class ActivePeriod(startHour: Int, finishHour: Int) {
   def includes(now: DateTime) = {
-    //TODO: this should systemClock()
-    val startOfDay = new DateTime(now.getYear, now.getMonthOfYear, now.getDayOfMonth, 0, 0, 0, 0)
+    val startOfDay = systemClock().date.toDateTimeAtStartOfDay
     now.isAfter(startOfDay.plusHours(startHour)) && now.isBefore(startOfDay.plusHours(finishHour))
   }
 }
