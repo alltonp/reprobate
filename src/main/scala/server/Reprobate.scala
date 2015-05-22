96aa5ac2267d2ef09dedc01eb9d37ee67afd3fcb
alltonp <paul.allton@theorem.technology>
2015-05-22 09:31:29 +0100

    a little featuring

--- src/main/scala/server/Reprobate.scala
+++ src/main/scala/server/Reprobate.scala
@@ -2,6 +2,11 @@ package server
 
 import im.mange.little.LittleServer
 
+//FEATURES:
+//use font awesome spinners instead of gifs
+//use font awesome icons instead of broadcast and config
+//read config from servers, shoreditch style
+//make broadcasts and config be live, and show the active/inactiveness of each check
 object Reprobate extends App {
   private val port = 8473
   new LittleServer(port)
