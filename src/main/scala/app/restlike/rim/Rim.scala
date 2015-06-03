b3ff2032d5e2e36090697060fdf0a5062285acaf
alltonp <paulallton@gmail.com>
2015-06-03 09:44:55 +0100

    make an appName in Rim

--- src/main/scala/app/restlike/rim/Rim.scala
+++ src/main/scala/app/restlike/rim/Rim.scala
@@ -8,11 +8,12 @@ import net.liftweb.http._
 object Rim extends RestHelper {
   import app.restlike.common.Responder._
 
-  //TODO: extract app name
+  val appName = "rim"
+
   serve {
-    case r@Req("rim" :: "install" :: Nil, _, GetRequest) ⇒ () ⇒ t(Script.install("rim"), downcase = false)
-    case r@Req("rim" :: "tracking" :: Nil, _, GetRequest) ⇒ () ⇒ t(Tracker("rim.tracking").view, downcase = false)
-    case r@Req("rim" :: "state" :: Nil, _, GetRequest) ⇒ () ⇒ Full(JsonResponse(Json.serialise(Persistence.load)))
-    case r@Req("rim" :: who :: Nil, _, PostRequest) ⇒ () ⇒ Controller.process(who, r)
+    case r@Req(`appName` :: "install" :: Nil, _, GetRequest) ⇒ () ⇒ t(Script.install(appName), downcase = false)
+    case r@Req(`appName` :: "tracking" :: Nil, _, GetRequest) ⇒ () ⇒ t(Tracker(s"$appName.tracking").view, downcase = false)
+    case r@Req(`appName` :: "state" :: Nil, _, GetRequest) ⇒ () ⇒ Full(JsonResponse(Json.serialise(Persistence.load)))
+    case r@Req(`appName` :: who :: Nil, _, PostRequest) ⇒ () ⇒ Controller.process(who, r)
   }
 }
