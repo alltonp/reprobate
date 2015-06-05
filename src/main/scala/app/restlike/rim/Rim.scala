fca536d1e9647c8c73a485c2ee6e74c9de1c24a7
alltonp <paul.allton@theorem.technology>
2015-06-05 17:39:56 +0100

    tidy a bit

--- src/main/scala/app/restlike/rim/Rim.scala
+++ src/main/scala/app/restlike/rim/Rim.scala
@@ -1,13 +1,10 @@
 package app.restlike.rim
 
-import app.restlike.common.{Tracker, Script}
+import app.restlike.common.{Script, Tracker}
 import net.liftweb.common.Full
 import net.liftweb.http._
 import net.liftweb.http.rest.RestHelper
-import org.json4s.JValue
 import org.json4s.native.JsonMethods
-
-//import net.liftweb.http._
 
 object Rim extends RestHelper {
   import app.restlike.common.Responder._
@@ -18,8 +15,7 @@ object Rim extends RestHelper {
     case r@Req(`appName` :: "install" :: Nil, _, GetRequest) ⇒ () ⇒ t(Script.install(appName), downcase = false)
     case r@Req(`appName` :: "tracking" :: Nil, _, GetRequest) ⇒ () ⇒ t(Tracker(s"$appName.tracking").view, downcase = false)
     case r@Req(`appName` :: "state" :: Nil, _, GetRequest) ⇒ () ⇒
-      val serialise: JValue = Json.serialise(Persistence.load)
-      Full(PlainTextResponse(JsonMethods.pretty(JsonMethods.render(serialise))))
+      Full(PlainTextResponse(JsonMethods.pretty(JsonMethods.render(Json.serialise(Persistence.load)))))
     case r@Req(`appName` :: who :: Nil, _, PostRequest) ⇒ () ⇒ Controller.process(who, r)
   }
 }
