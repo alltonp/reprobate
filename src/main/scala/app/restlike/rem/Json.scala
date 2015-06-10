a98561b33f87151238071217b09ddd0032edc8ef
alltonp <paulallton@gmail.com>
2015-06-10 09:38:34 +0100

    local backup of just the state for the supplied token

--- src/main/scala/app/restlike/rem/Json.scala
+++ src/main/scala/app/restlike/rem/Json.scala
@@ -15,4 +15,9 @@ object Json {
     implicit val formats = theFormats
     JsonParser.parse(write(response))
   }
+
+  def serialise(response: Option[Model]) = {
+    implicit val formats = theFormats
+    JsonParser.parse(write(response))
+  }
 }
--- src/main/scala/app/restlike/rem/Rem.scala
+++ src/main/scala/app/restlike/rem/Rem.scala
@@ -13,7 +13,7 @@ object Rem extends RestHelper {
   serve {
     case r@Req(`appName` :: "install" :: token :: Nil, _, GetRequest) ⇒ () ⇒ t(Script.install(appName, token), downcase = false)
     case r@Req(`appName` :: "tracking" :: token :: Nil, _, GetRequest) ⇒ () ⇒ t(Tracker(s"${appName}.tracking").view, downcase = false)
-    case r@Req(`appName` :: "state" :: token :: Nil, _, GetRequest) ⇒ () ⇒ Full(JsonResponse(Json.serialise(Persistence.load)))
+    case r@Req(`appName` :: "state" :: token :: Nil, _, GetRequest) ⇒ () ⇒ Full(JsonResponse(Json.serialise(Persistence.load.modelFor(token))))
     case r@Req(`appName` :: who :: token :: Nil, _, PostRequest) ⇒ () ⇒ Controller.process(who, r, token)
   }
 }
