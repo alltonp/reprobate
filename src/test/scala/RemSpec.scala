58bf3286b69c3c19a6573950ce7dad1607908ad7
alltonp <paul.allton@theorem.technology>
2015-06-12 11:13:09 +0100

    more tidy up

--- src/test/scala/RemSpec.scala
+++ src/test/scala/RemSpec.scala
@@ -47,25 +47,6 @@ class RemSpec extends WordSpec with MustMatchers {
     runAndExpect("1 _= new value", current, expected)
   }
 
-
-  //  "add and move forward to begin state" in {
-//    val current = emptyModelWithWorkflow
-//    val expected = current.copy(issues = List(Issue("1", "an item", Some(next), None)))
-//    runAndExpect("+/ an item", current, expected)
-//  }
-
-//  "add and move forward to second state" in {
-//    val current = emptyModelWithWorkflow
-//    val expected = current.copy(issues = List(Issue("1", "an item", Some(doing), Some(aka))))
-//    runAndExpect("+// an item", current, expected)
-//  }
-
-//  "add and move forward to end state" in {
-//    val current = emptyModelWithWorkflow
-//    val expected = current.copy(issues = List(Issue("1", "an item", Some(done), Some(aka))))
-//    runAndExpect("+! an item", current, expected)
-//  }
-
   "add with tags" in {
     val current = emptyModelWithWorkflow
     val expected = current.copy(things = List(Thing("1", "key", Some("value"), Set("tag1", "tag2"))))
@@ -83,117 +64,6 @@ class RemSpec extends WordSpec with MustMatchers {
     val expected = current.copy(things = List(Thing("1", "key", None, Set("tag1", "tag2"))))
     runAndExpect("+ key : :tag1 :tag2", current, expected)
   }
-
-//  "add and move forward to begin state with tags" in {
-//    val current = emptyModelWithWorkflow
-//    val expected = current.copy(issues = List(Issue("1", "an item", Some(next), None, Set("tag1", "tag2"))))
-//    runAndExpect("+/ an item : tag1 tag2", current, expected)
-//  }
-
-//  "add and move forward to end state with tags" in {
-//    val current = emptyModelWithWorkflow
-//    val expected = current.copy(issues = List(Issue("1", "an item", Some(done), Some(aka), Set("tag1", "tag2"))))
-//    runAndExpect("+! an item : tag1 tag2", current, expected)
-//  }
-
-  //moving
-
-//  "move forward one state" in {
-//    val issue = Issue("1", "an item", Some(doing), None)
-//    val current = modelWithIssue(issue)
-//    val expected = current.copy(issues = List(issue.copy(status = Some(done), by = Some(aka))))
-//    runAndExpect("1 /", current, expected)
-//  }
-
-  //TODO: this is not completely trivial, e.g. going past done etc
-//  "move forward two states" in {
-//    val issue = Issue("1", "an item", Some(doing), None)
-//    val current = modelWithIssue(issue)
-//    val expected = current.copy(issues = List(issue.copy(status = Some(done), by = Some(aka))))
-//    runAndExpect("1 //", current, expected)
-//  }
-
-//  "move forward to an initial leaves disowned" in {
-//    val issue = Issue("1", "an item", None, None)
-//    val current = modelWithIssue(issue)
-//    val expected = current.copy(issues = List(issue.copy(status = Some(next))))
-//    runAndExpect("1 /", current, expected)
-//  }
-
-//  "move forward two states" in {
-//    (pending)
-//    val issue = Issue("1", "an item", None, None)
-//    val current = modelWithIssue(issue)
-//    val expected = current.copy(issues = List(issue.copy(status = Some(doing), by = Some(aka))))
-//    runAndExpect("1 //", current, expected)
-//  }
-
-//  "move forward to end state" in {
-//    val issue = Issue("1", "an item", None, None)
-//    val current = modelWithIssue(issue)
-//    val expected = current.copy(issues = List(issue.copy(status = Some(done), by = Some(aka))))
-//    runAndExpect("1 /!", current, expected)
-//  }
-
-//  "move back a state" in {
-//    val issue = Issue("1", "an item", Some(done), None)
-//    val current = modelWithIssue(issue)
-//    val expected = current.copy(issues = List(issue.copy(status = Some(doing), by = Some(aka))))
-//    runAndExpect("1 .", current, expected)
-//  }
-
-//  "move back a state to begin state" in {
-//    val issue = Issue("1", "an item", Some(doing), None)
-//    val current = modelWithIssue(issue)
-//    val expected = current.copy(issues = List(issue.copy(status = Some(next), by = None)))
-//    runAndExpect("1 .", current, expected)
-//  }
-
-  //TODO: by should be None
-//  "move back a state (into backlog)" in {
-//    val issue = Issue("1", "an item", Some(next), None)
-//    val current = modelWithIssue(issue)
-//    val expected = current.copy(issues = List(issue.copy(status = None, by = None)))
-//    runAndExpect("1 .", current, expected)
-//  }
-
-//  "move back to begin state (into backlog)" in {
-//    val issue = Issue("1", "an item", Some(done), None)
-//    val current = modelWithIssue(issue)
-//    val expected = current.copy(issues = List(issue.copy(status = None, by = None)))
-//    runAndExpect("1 .!", current, expected)
-//  }
-
-  //owning
-
-//  "own" in {
-//    val issue = Issue("1", "an item", Some(next), None)
-//    val current = modelWithIssue(issue)
-//    val expected = current.copy(issues = List(issue.copy(by = Some(aka))))
-//    runAndExpect("1 @", current, expected)
-//  }
-
-//  "disown" in {
-//    val issue = Issue("1", "an item", Some(next), Some(aka))
-//    val current = modelWithIssue(issue)
-//    val expected = current.copy(issues = List(issue.copy(by = None)))
-//    runAndExpect("1 @-", current, expected)
-//  }
-
-//  "assign" in {
-//    val issue = Issue("1", "an item", Some(next), Some(aka))
-//    val current = modelWithIssue(issue)
-//    val expected = current.copy(issues = List(issue.copy(by = Some(aka2))))
-//    runAndExpect("1 @= b", current, expected)
-//  }
-
-//  "assign (invalid aka)" in {
-//    (pending) //TODO: TODO: need to start asserting the Out().messages
-//    val issue = Issue("1", "an item", Some(next), Some(aka))
-//    val current = modelWithIssue(issue)
-//    val expected = current.copy(issues = List(issue.copy(by = Some(aka2))))
-//    runAndExpect("1 @= c", current, expected)
-//  }
 
   //tagging
 
@@ -237,17 +107,6 @@ class RemSpec extends WordSpec with MustMatchers {
 //    runAndExpect("tag1 := tagX", current, expected)
 //  }
 
-  //show
-
-//  "show board" in {
-//    (pending) //TODO: need to start asserting the Out().messages
-//    val issue = Issue("1", "an item", Some(done), None)
-//    val current = modelWithIssue(issue)
-//    val expected = current.copy(issues = List(issue.copy(status = None, by = None)))
-//    runAndExpect("", current, expected)
-//  }
-
-
   private def runAndExpect(in: String, current: Model, expected: Model) {
     run(s"$in", current).updatedModel.mustEqual(Some(expected))
   }
@@ -255,5 +114,4 @@ class RemSpec extends WordSpec with MustMatchers {
   private def run(in: String, current: Model) = Commander.process(in, "anon", current, RefProvider(0), "foo@bar.com")
 
   private def modelWithThing(issue: Thing) = Model(/*usersToAka, */List(issue))
-//  private def modelWithReleasedIssue(issue: Thing) = Model(workflowStates, usersToAka, Nil, List(Release("release", List(issue))))
 }
