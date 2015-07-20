176f0aedbc54fe164eee3768dfa76bbe1bd8fd03
alltonp <paulallton@gmail.com>
2015-07-20 12:04:35 +0100

    hide all tracking endpoints for now

--- src/main/scala/app/restlike/rim/Rim.scala
+++ src/main/scala/app/restlike/rim/Rim.scala
@@ -17,8 +17,8 @@ object Rim extends RestHelper {
     case r@Req(`appName` :: "install" :: token :: Nil, _, GetRequest) ⇒ () ⇒
       t(Script.install(appName, token), downcase = false)
     //TODO: this should be filtered for the token
-    case r@Req(`appName` :: "tracking" :: token :: Nil, _, GetRequest) ⇒ () ⇒
-      t(Tracker(s"$appName.tracking").view, downcase = false)
+//    case r@Req(`appName` :: "tracking" :: token :: Nil, _, GetRequest) ⇒ () ⇒
+//      t(Tracker(s"$appName.tracking").view, downcase = false)
     case r@Req(`appName` :: "state" :: token :: Nil, _, GetRequest) ⇒ () ⇒
       Full(PlainTextResponse(JsonMethods.pretty(JsonMethods.render(Json.serialise(Persistence.load.modelFor(token))))))
     case r@Req(`appName` :: who :: token :: Nil, _, PostRequest) ⇒ () ⇒
