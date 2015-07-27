6788fa449395347e86cd4ee4049961cff86fd4e1
alltonp <paulallton@gmail.com>
2015-07-27 23:41:48 +0100

    en-redden blocks issues

--- src/main/scala/app/restlike/rim/Model.scala
+++ src/main/scala/app/restlike/rim/Model.scala
@@ -35,6 +35,7 @@ case class Issue(ref: String, description: String, status: Option[String], by: O
     model.fold(value)(m =>
       status match {
         case None => customGrey(value)
+        case Some(x) if blocked.isDefined => red(value)
         case Some(x) if x == m.beginState => customYellow(value)
         case Some(x) if x == m.endState => customGreen(value)
         case Some("released") => customMagenta(value)
