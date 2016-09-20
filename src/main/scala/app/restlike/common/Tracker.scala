ad6c3c5bd34935e68055992b1360b2501ad5eaa4
pall <paulallton@gmail.com>
2016-09-20 22:14:01 +0100

    put token first, so we can add extra params

--- src/main/scala/app/restlike/common/Tracker.scala
+++ src/main/scala/app/restlike/common/Tracker.scala
@@ -9,17 +9,17 @@ import org.joda.time.DateTime
 //actually, can have generic History, but extned/wrap for the rim specific bits
 case class History(content: String) {
   private val contentBits = content.split("\\|")
-  private val what = contentBits.lift(2)
+  private val what = contentBits.lift(3)
   private val whatBits = what.map(_.split(" "))
 
 //  try {
     val ref = whatBits.flatMap(_.lift(0))
     val action = whatBits.flatMap(_.lift(1))
-    val when = contentBits.lift(0).map(ms => new DateTime(ms.toLong))
-    val who = contentBits.lift(1)
-    val token = contentBits.lift(3)
+    val when = contentBits.lift(1).map(ms => new DateTime(ms.toLong))
+    val who = contentBits.lift(2)
+    val token = contentBits.lift(0)
 
-  val printable = contentBits.reverse.drop(1).reverse.mkString("|")
+    val printable = contentBits.drop(1).mkString("|")
 //    println(s"$content - $ref $email $who $action")
 
 //  } catch {
@@ -33,7 +33,7 @@ case class Tracker(filename: String) {
   private val file = Paths.get(filename)
 
   def track(who: String, what: String, token: String) {
-    val content = List(DateTime.now.getMillis, who, what, token).mkString("|") + "\n"
+    val content = List(token, DateTime.now.getMillis, who, what).mkString("|") + "\n"
     Filepath.append(content, file)
   }
 
