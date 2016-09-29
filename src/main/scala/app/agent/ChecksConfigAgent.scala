ce1cc48a2ff65affd19aaf4c45ce7508247918ba
alltonp <paulallton@gmail.com>
2016-09-29 01:37:13 +0100

    get it loading again

--- src/main/scala/app/agent/ChecksConfigAgent.scala
+++ src/main/scala/app/agent/ChecksConfigAgent.scala
@@ -20,6 +20,6 @@ case class ChecksConfigAgent(subscriber: Subscriber) extends Renderable {
 
   def render = holder.render
   def requestSummary = holder.show & holder.fill(BigSpinner("checksConfigSpinner", "Loading checks config..."))
-  def show(response: ProbeConfigResponse) = configEditorAgent.onLoad(response.config) & holder.fill(R(configEditorAgent))
+  def show(response: ProbeConfigResponse) = holder.fill(R(configEditorAgent)) & configEditorAgent.onLoad(response.config)
   def hide = holder.empty & holder.hide
 }
