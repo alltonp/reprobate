e328bbf837f940d7c56ed4e48d7ff86b3ffda94a
alltonp <paulallton@gmail.com>
2018-12-07 15:34:42 +0000

    first stab at dockering up the build

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,7 @@
 FROM openjdk:8-jre-alpine
+
+COPY ./target/scala-2.11/reprobate-assembly-1.0-SNAPSHOT.jar /root/reprobate.jar
+
+EXPOSE 8473
+
+CMD ["sh", "-c", "java -Xss8m -Xmx512m -jar /root/reprobate.jar"]
--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-sbt dist
+sbt dist assembly
+#docker build --no-cache  -t reprobate-`date + "%T"` .
+docker build --no-cache  -t reprobate-1 .
+
+docker stop reprobate-current ||: docker rm reprobate-current ||:
+
+docker run --name reprobate-current -t -d \
+-p 8473:8473 \
+reprobate-1
