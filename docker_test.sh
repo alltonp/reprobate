8c33c6c2d500d6d4522f27ca83955d266eb38727
pall <paulallton@gmail.com>
2019-01-29 08:59:54 +0000

    capturing more useful stuff

--- docker_test.sh
+++ docker_test.sh
@@ -5,12 +5,16 @@ docker build --no-cache  -t reprobate-1 .
 docker stop reprobate-current ||:
 docker rm reprobate-current ||:
 rm -rf /Users/pall/dev/paulos/reprobate/test-data
-docker run --name reprobate-current -t -d -h=`hostname` \
+docker run --name reprobate-current -t spabloshi/reprobate -d -h=`hostname` \
 -v /Users/pall/dev/paulos/reprobate/test-data:/data \
 -p 8473:8473 \
 reprobate-1
 
 #issues:
 #need to do non root thing
-#checks executed going to 0 on config change, not right ... state.json
+#checks executed resetting to 0 on dynamic config change, not right ... state.json
 #app.log should go into data too
+
+#docker login
+#docker tag reprobate-1 paulallton/reprobate_alpha
+#docker push spabloshi/reprobate
